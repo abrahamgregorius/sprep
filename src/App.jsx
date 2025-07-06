c964452a49c4d88ae6734309428597d1d90127a1
abrahamgregorius <athioii6@gmail.com>
2025-07-06 22:29:07 +0700

    remove menu

--- src/App.jsx
+++ src/App.jsx
@@ -99,11 +99,11 @@ export default function App() {
               Pricing
             </a>
           </div>
-          <button className="md:hidden text-white">
+          {/* <button className="md:hidden text-white">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
             </svg>
-          </button>
+          </button> */}
         </div>
       </nav>
 
